e6b7441e74ac9558ece2e0e198bfa216c3375d09
Amjad Jibon <amjad.jibon@gmail.com>
2021-10-22 01:18:38 +0600

    use mod add

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,18 @@ mod print;
 mod city;
 mod colors;
 
+use print::{hello, comment, fmtprint, fmtprint2, fmtprint3, debugprint, complexprint, points};
+
 fn main() {
-    print::hello();
-    print::comment();
-    print::fmtprint(String::from("Amjad"));
-    print::fmtprint("Jibon".to_string());
-    print::fmtprint2("Khan");
-    print::fmtprint3("Hossain");
-    print::debugprint();
-    print::points();
-    print::complexprint(3.5, 4.8);
+    hello();
+    comment();
+    fmtprint(String::from("Amjad"));
+    fmtprint("Jibon".to_string());
+    fmtprint2("Khan");
+    fmtprint3("Hossain");
+    debugprint();
+    points();
+    complexprint(3.5, 4.8);
     city::dispcity("Dhaka", 190.122345, -32.543216);
     colors::disprgb(255, 110, 25)
 }
