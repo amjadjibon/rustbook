54bdae0cddff80cc5a127bbca7bb2a6e56ac78bd
Amjad Jibon <amjad.jibon@gmail.com>
2022-10-09 16:33:59 +0600

    lifetimes

---
+++ lifetimes/src/main.rs
@@ -0,0 +1,168 @@
+// fn main() {
+//     let r;                // ---------+-- 'a
+//                           //          |
+//     {                     //          |
+//         let x = 5;        // -+-- 'b  |
+//         r = &x;           //  |       |
+//     }                     // -+       |
+//                           //          |
+//     println!("r: {}", r); //          |
+// }
+
+// fn main() {
+//     let x = 5;            // ----------+-- 'b
+//                           //           |
+//     let r = &x;           // --+-- 'a  |
+//                           //   |       |
+//     println!("r: {}", r); //   |       |
+//                           // --+       |
+// }                         // ----------+
+
+use std::fmt::Display;
+
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+
+    let string1 = String::from("long string is long");
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
+
+    // let string1 = String::from("long string is long");
+    // let result;
+    // {
+    //     let string2 = String::from("xyz");
+    //     result = longest(string1.as_str(), string2.as_str());
+    // }
+    // println!("The longest string is {}", result);
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.')
+        .next()
+        .expect("Could not find a '.'");
+
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+
+    println!("i.part: {}", i.part);
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.')
+        .next()
+        .expect("Could not find a '.'");
+
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+
+    println!("i.part: {}", i.return_part("hello"));
+
+    let string1 = String::from("long string is long");
+    let result;
+    {
+        let string2 = String::from("xyz");
+        result = longest_with_an_announcement(
+            string1.as_str(),
+            string2.as_str(),
+            "hello"
+        );
+
+        println!("The longest string is {}", result);
+    }
+
+    // println!("The longest string is {}", result);
+
+}
+
+// fn longest(x: &str, y: &str) -> &str {
+//     if x.len() > y.len() {
+//         x
+//     } else {
+//         y
+//     }
+// }
+
+// &i32        // a reference
+// &'a i32     // a reference with an explicit lifetime
+// &'a mut i32 // a mutable reference with an explicit lifetime
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+// fn longest2(x: &str, y: &str) -> &str {
+//     let result = String::from("really long string");
+//     result.as_str()
+// }
+
+fn longest3(x: &str, y: &str) -> String {
+    let result = String::from("really long string");
+    result
+}
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl <'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+
+    fn return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn first_word2<'a>(s: &'a str) -> &'a str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn longest_with_an_announcement<'a, T> (
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+where
+    T: Display
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
